ba39d64d4edbd611941b549996588aa13c1227c3
davipythonweb <galaxys3tab32@gmail.com>
2024-06-07 21:30:02 -0300

    update url `error`

--- phones/urls.py
+++ phones/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import phones
+from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 
 
 urlpatters = [
-    path('', phones, name='phones_list'),
+    path('', views.PhonesListView, name='phones_list'),
 ] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
