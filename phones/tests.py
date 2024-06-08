a566a3174e8c9ce172bf887181e08dcdc4254f34
davipythonweb <galaxys3tab32@gmail.com>
2024-06-07 23:26:35 -0300

    tests unitest `tests.py`

--- phones/tests.py
+++ phones/tests.py
@@ -1,9 +1,13 @@
 from django.test import TestCase
-
+from django.urls import reverse
 
 class PhonesURLsTest(TestCase):  
     def test_pytest_is_ok(self):
         assert 1 == 1, 'um rh igual a um.'
+        
+    # def test_phones_home_url_is_correct(self):
+    #     url = reverse('phones:phones_list')
+    #     self.assertEqual(url, '/')
         
         
     
