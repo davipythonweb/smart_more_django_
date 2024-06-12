1f079d6ecb8241d9f80bc917bdf43a18176a7603
davipythonweb <galaxys3tab32@gmail.com>
2024-06-11 22:25:15 -0300

    update project

--- phones/tests.py
+++ phones/tests.py
@@ -3,7 +3,7 @@ from django.urls import reverse
 
 class PhonesURLsTest(TestCase):  
     def test_pytest_is_ok(self):
-        assert 1 == 1, 'um rh igual a um.'
+        assert 1 == 1, 'um eh igual a um.'
         
     def test_phones_list_url_is_correct(self):
         url = reverse('namespace:list')
@@ -14,7 +14,7 @@ class PhonesURLsTest(TestCase):
         self.assertEqual(url, '/new_phone/')
         
     def test_phones_detail_url_is_correct(self):
-        url = reverse('namespace:detail', kwargs={'pk': 1})
+        url = reverse('namespace:detail', kwargs={'pk': 1}) 
         self.assertEqual(url, '/phone/1')
         
     def test_phones_update_url_is_correct(self):
